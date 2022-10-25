f7e6bc57545ebd55467e7e7eb3ba5eda67eb7a4b
diegookawa <diego.arenas1505@gmail.com>
2022-10-25 14:37:56 -0300

    Cadastra cliente e adiciona compromisso

--- cliente.py
+++ cliente.py
@@ -4,30 +4,82 @@ import threading
 
 @Pyro5.api.expose
 @Pyro5.api.callback
-
 class cliente_callback(object):
+
+    def __init__(self, nome) -> None:
+        self.nome = nome
+        self.public_key = ""
+        self.referenciaCliente = ""
+
     def notificacao(self, msg):
         print("callback recebido do servidor!")
+
+    def receberMensagemCompromisso(self, msg):
+        option = int(input(msg))
+        
+        return option
 
     def loopThread(daemon):
         #thread para ficar escutando chamadas de método do servidor
         daemon.requestLoop()
 
+    def getNome(self):
+        return self.nome
+
+    def setPublic_key(self, public_key):
+        cliente_callback.public_key = public_key
+
+    def cadastrarCompromisso(self):
+        compromisso = {}
+        convidadosCompromisso = None
+        nomeCompromisso = input("Informe o nome do compromisso: ")
+        dataCompromisso = input("Informe a data do compromisso: ")
+        horarioCompromisso = input("Informe o horario do compromisso: ")
+
+        optionConvidados = int(input("Deseja adicionar convidados?\n1 - Sim\n2 - Não\n"))
+        if optionConvidados != 2:
+            convidadosCompromisso = input("Informe o nome dos convidados separados por ,: ")
+
+        optionAlerta = int(input("Deseja receber o alerta?\n1 - Sim\n2 - Não\n"))
+        if optionAlerta != 2:
+            horarioAlerta = input("Informe o horário do alerta: ")
+            compromisso["horarioAlerta"] = horarioAlerta
+
+        compromisso["nome"] = nomeCompromisso
+        compromisso["data"] = dataCompromisso
+        compromisso["horario"] = horarioCompromisso
+        
+        return compromisso, convidadosCompromisso
+
 def main():
-    name = input("What is your name? ").strip()
-    ns = Pyro5.api.locate_ns()  
-    daemon = Pyro5.server.Daemon() 
-    servidor = Pyro5.api.Proxy("PYRONAME:Agenda")
-    referenciaCliente = daemon.register(cliente_callback)
+    with Pyro5.api.Daemon() as daemon:
 
-    #Invoca método no servidor, passando a referência
-    public_key = servidor.cadastro(referenciaCliente, name)
-    print(public_key)
+        nome = input("Informe seu nome: ").strip()
+        callback = cliente_callback(nome)
+        callback.referenciaCliente = daemon.register(callback)
 
-    #Inicializa a thread para receber notificações do servidor
-    thread = threading.Thread(target=cliente_callback.loopThread, args=(daemon,))
-    thread.daemon = True
-    thread.start()
+        #Inicializa a thread para receber notificações do servidor
+        thread = threading.Thread(target=cliente_callback.loopThread, args=(daemon,))
+        thread.daemon = True
+        thread.start()
+
+        ns = Pyro5.api.locate_ns()  
+        uri = ns.lookup("Agenda")
+        servidor = Pyro5.api.Proxy(uri)
+
+        #Invoca método no servidor, passando a referência
+        servidor.cadastro(callback.referenciaCliente, callback.nome)
+    
+        option = -1
+        while option != 5:
+            option = int(input("Informe uma opcao:\n1 - Cadastrar compromisso\n2 - Cancelar compromisso\n3 - Consultar compromisso\n4 - Atualizar\n5 - Sair\n"))
+
+            if option == 1:
+                compromisso, convidadosCompromisso = callback.cadastrarCompromisso()
+                servidor.cadastrarCompromisso(callback.nome, compromisso, convidadosCompromisso)
+
+            # elif option == 2:
+            #     servidor
 
 if __name__ == "__main__":
     main()
--- servidor.py
+++ servidor.py
@@ -5,22 +5,43 @@ from Crypto.PublicKey import RSA
 @Pyro5.api.expose
 class Servidor(object):
 
-    clientes = []
+    clientes = {}
+    compromissos = []
 
+    @Pyro5.server.oneway
     def cadastro(self, referenciaCliente, nome):
+        cliente = Pyro5.api.Proxy(referenciaCliente)
         key = RSA.generate(2048)
         public_key = key.publickey().exportKey("PEM")
         private_key = key.exportKey("PEM")
+        Servidor.clientes[nome] = (referenciaCliente, private_key)
 
-        self.clientes.append((nome, referenciaCliente, private_key))
-        print(self.clientes)
+        cliente.setPublic_key(public_key)
 
-        return public_key
+    @Pyro5.server.oneway
+    def cadastrarCompromisso(self, nome, compromisso, convidadosCompromisso):
+        Servidor.compromissos.append((nome, compromisso))
 
-daemon = Pyro5.server.Daemon()         # make a Pyro daemon
-ns = Pyro5.api.locate_ns()             # find the name server
-uri = daemon.register(Servidor)        # register the greeting maker as a Pyro object
-ns.register("Agenda", uri)             # register the object with a name in the name server
+        if(convidadosCompromisso is not None):
+            convidados = convidadosCompromisso.split(",")
+            nomeCompromisso = compromisso["nome"]
 
-print("Ready.")
-daemon.requestLoop()                   # start the event loop of the server to wait for calls
+            for convidado in convidados:
+                if(Servidor.clientes.get(convidado) is not None):
+                    callbackConvidado = Pyro5.api.Proxy(Servidor.clientes[convidado][0])
+                    option = callbackConvidado.receberMensagemCompromisso(f"Deseja participar do compromisso: {nomeCompromisso}?\n1 - Sim\n2 - Não\n")
+                    
+                    if option == 1:
+                        Servidor.compromissos.append((callbackConvidado.getNome(), compromisso))
+
+def main():
+    daemon = Pyro5.server.Daemon()         # make a Pyro daemon
+    ns = Pyro5.api.locate_ns()             # find the name server
+    uri = daemon.register(Servidor)        # register the greeting maker as a Pyro object
+    ns.register("Agenda", uri)             # register the object with a name in the name server
+
+    print("Ready.")
+    daemon.requestLoop()                   # start the event loop of the server to wait for calls
+
+if __name__ == "__main__":
+    main()
